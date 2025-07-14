52f2a1f67fe90cb9b000ffcfed94e75303d09331
TPNYC\nodira.kazakova <nodira.kazakova@transperfect.com>
2025-07-14 10:16:15 -0400

    IAccountRepository added

---
+++ Interface/IAccountRepository.cs
@@ -0,0 +1,11 @@
+using banking_api_repo.Models.Requests;
+using banking_api_repo.Models.Responses;
+
+namespace banking_api_repo.Interface;
+
+public interface IAccountRepository
+{
+    public Task<AccountResponse> AddAccount(AccountResponse accountResponse);
+    public Task<AccountResponse> UpdateAccount(AccountResponse accountResponse);
+    public Task<AccountResponse> GetAccountById(AccountRequest accountRequest);
+}
