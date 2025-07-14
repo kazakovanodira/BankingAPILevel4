f4522d94aa30b28750441d1a9dd91f5bce79cd16
TPNYC\nodira.kazakova <nodira.kazakova@transperfect.com>
2025-07-14 11:42:48 -0400

    account repository updated

---
+++ Repositories/AccountRepository.cs
@@ -0,0 +1,66 @@
+using banking_api_repo.Data;
+using banking_api_repo.Interface;
+using banking_api_repo.Models;
+using banking_api_repo.Models.Requests;
+using banking_api_repo.Models.Responses;
+
+namespace banking_api_repo.Repositories;
+
+public class AccountRepository : IAccountRepository
+{
+    private readonly AccountsContext _context;
+    
+    public AccountRepository(AccountsContext context)
+    {
+        _context = context;
+    }
+    
+    public Task<AccountResponse> AddAccount(AccountResponse accountResponse)
+    {
+        var account = new Account
+        {
+            AccountNumber = accountResponse.AccountId,
+            Balance = accountResponse.Balance,
+            Name = accountResponse.Name
+
+        };
+        
+        _context.Accounts.Add(account);
+        _context.SaveChanges();
+
+        return Task.FromResult(new AccountResponse(account.AccountNumber, account.Name, account.Balance));
+    }
+
+    public Task<AccountResponse>? UpdateAccount(AccountResponse accountResponse)
+    {
+        var account = _context.Accounts.FirstOrDefault(account => 
+            account.AccountNumber == accountResponse.AccountId);
+        
+        if (account is null)
+        {
+            return null;
+        }
+        
+        account.AccountNumber = accountResponse.AccountId;
+        account.Balance = accountResponse.Balance;
+        account.Name = accountResponse.Name;
+        
+        _context.Accounts.Add(account);
+        _context.SaveChanges();
+
+        return Task.FromResult(new AccountResponse(account.AccountNumber, account.Name, account.Balance));
+    }
+
+    public Task<AccountResponse>? GetAccountById(AccountRequest accountRequest)
+    {
+        var account = _context.Accounts.FirstOrDefault(account => 
+            account.AccountNumber == accountRequest.AccountId);
+        
+        if (account is null)
+        {
+            return null;
+        }
+
+        return Task.FromResult(new AccountResponse(account.AccountNumber, account.Name, account.Balance));
+    }
+}
