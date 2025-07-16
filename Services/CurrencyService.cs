2c8c950f474a559b7789555c131a9a59cc2725d5
TPNYC\nodira.kazakova <nodira.kazakova@transperfect.com>
2025-07-16 15:47:58 -0400

    json deserialization improved

--- Services/CurrencyService.cs
+++ Services/CurrencyService.cs
@@ -10,13 +10,14 @@ public class CurrencyService : ICurrencyServices
     {
         using (HttpClient client = new HttpClient())
         {
-            var endpoint = new Uri("https://api.freecurrencyapi.com/v1/latest?apikey=fca_live_bC3MRgGRkcSUkEpNW7dQJ8bNqboiMe2f33CL3F4V?currencies=EUR,CAD");
+            var endpoint = new Uri("https://api.freecurrencyapi.com/v1/latest?apikey=fca_live_bC3MRgGRkcSUkEpNW7dQJ8bNqboiMe2f33CL3F4V&currencies=EUR,CAD");
             var jsonString = await client.GetStringAsync(endpoint);
-            Console.WriteLine(jsonString);
             
-            CurrencyResponse currencyResponse = JsonSerializer.Deserialize<CurrencyResponse>(jsonString) ?? throw new NullReferenceException();
-            
-            //Dictionary<string, decimal> fetchedCurrencies = JsonSerializer.Deserialize<Dictionary<string, decimal>>(jsonString) ?? new Dictionary<string, decimal>();
+            CurrencyResponse currencyResponse = JsonSerializer.Deserialize<CurrencyResponse>(jsonString,
+                new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
+                }) ?? throw new NullReferenceException();
             
             return currencyResponse.Data;
         }
