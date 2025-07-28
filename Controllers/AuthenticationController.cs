cabdf7a66bd9fafeed8d06f2123753a1bcccb440
TPNYC\nodira.kazakova <nodira.kazakova@transperfect.com>
2025-07-27 23:19:49 -0400

    requiring a token added

--- Controllers/AuthenticationController.cs
+++ Controllers/AuthenticationController.cs
@@ -1,7 +1,6 @@
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using banking_api_repo.Interface;
-using banking_api_repo.Models.Requests;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
 
