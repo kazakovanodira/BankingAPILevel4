31f0e324b8a042d87723558effe419650e8e46e1
Nodira Kazakova <kazakovanodira04@gmail.com>
2025-07-29 15:27:52 -0400

    validate token added

---
+++ WebAppBuilderExtensions.cs
@@ -0,0 +1,45 @@
+using System.Text;
+using banking_api_repo.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
+namespace banking_api_repo;
+
+public static class WebAppBuilderExtensions
+{
+    public static WebAppBuilderExtensions RegisterAuthentication()
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        var jwtSettings = new JwtSettings();
+        builder.Configuration.Bind(nameof(JwtSettings), jwtSettings);
+
+        var jwtSection = builder.Configuration.GetSection(nameof(JwtSettings));
+        builder.Services.Configure<JwtSettings>(jwtSection);
+
+        builder.Services.AddAuthentication(a =>
+        {
+            a.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            a.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            a.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        }).AddJwtBearer(jwt =>
+        {
+            jwt.SaveToken = true;
+            jwt.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(jwtSettings.SigningKey
+                    ?? throw new InvalidOperationException())),
+                ValidateIssuer = true,
+                ValidIssuer = jwtSettings.Issuer,
+                ValidateAudience = true,
+                ValidAudiences = jwtSettings.Audiences,
+                RequireExpirationTime = false,
+                ValidateLifetime = true
+            };
+            jwt.Audience = jwtSettings.Audiences?[0];
+            jwt.ClaimsIssuer = jwtSettings.Issuer;
+        });
+
+    }
+}
