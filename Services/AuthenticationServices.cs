cc1550cd2a886b6994a2159f82de4d039e3a45f1
Nodira Kazakova <kazakovanodira04@gmail.com>
2025-07-29 14:54:08 -0400

    Authentication services constructor added

--- Services/AuthenticationServices.cs
+++ Services/AuthenticationServices.cs
@@ -1,8 +1,23 @@
+using System.Text;
 using banking_api_repo.Interface;
+using banking_api_repo.Models;
+using Microsoft.Extensions.Options;
 
 namespace banking_api_repo.Services;
 
 public class AuthenticationServices : IAuthenticationServices
 {
-    
+    private readonly JwtSettings? _settings;
+    private readonly byte[] _key;
+
+    public AuthenticationServices(IOptions<JwtSettings> jwtOptions)
+    {
+        _settings = jwtOptions.Value;
+        ArgumentNullException.ThrowIfNull(_settings);
+        ArgumentNullException.ThrowIfNull(_settings.SigningKey);
+        ArgumentNullException.ThrowIfNull(_settings.Audiences);
+        ArgumentNullException.ThrowIfNull(_settings.Audiences[0]);
+        ArgumentNullException.ThrowIfNull(_settings.Issuer);
+        _key = Encoding.ASCII.GetBytes(_settings?.SigningKey!);
+    }
 }
