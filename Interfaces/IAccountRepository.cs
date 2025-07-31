8f129416ba7eb05367d8c2cd7a8778e192fbf900
Nodira Kazakova <kazakovanodira04@gmail.com>
2025-07-31 11:22:46 -0400

    stylistic changes

--- Interfaces/IAccountRepository.cs
+++ Interfaces/IAccountRepository.cs
@@ -1,14 +1,13 @@
 using banking_api_repo.Models;
 using banking_api_repo.Services;
-using Microsoft.AspNetCore.Identity;
 
-namespace banking_api_repo.Interface;
+namespace banking_api_repo.Interfaces;
 
 public interface IAccountRepository
 {
     Task<User> AddAccount(User user);
     Task<User?> UpdateAccount(User user, decimal amount);
-    Task<User?> GetAccountByAccountNumber(Guid AccountNumber);
+    Task<User?> GetAccountByAccountNumber(Guid accountNumber);
     Task<User?> GetAccountByUserName(string username);
     Task<IEnumerable<User>> GetAccountsAsync();
     Task<(IEnumerable<User>, PaginationMetadata)> GetAccountsAsync(string? name, 
