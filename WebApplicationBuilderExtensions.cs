1c9469d9d85034b44d4d3dddc7c8f3d85bbe7226
Nodira Kazakova <kazakovanodira04@gmail.com>
2025-08-01 12:51:03 -0400

    swagger documentation for authorization added

--- WebApplicationBuilderExtensions.cs
+++ WebApplicationBuilderExtensions.cs
@@ -4,6 +4,7 @@ using banking_api_repo.Models;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 
 namespace banking_api_repo;
 
@@ -64,5 +65,37 @@ public static class WebApplicationBuilderExtensions
             .AddEntityFrameworkStores<UserContext>();
 
         return builder;
+    }
+
+    public static IServiceCollection AddSwagger(this IServiceCollection services)
+    {
+        services.AddSwaggerGen(options =>
+        {
+            options.SwaggerDoc("v1", new OpenApiInfo { Title = "Simple Bank API", Version = "v1" });
+            options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                In = ParameterLocation.Header,
+                Description = "Please enter a valid token.",
+                Name = "Authorization",
+                Type = SecuritySchemeType.Http,
+                BearerFormat = "JWT",
+                Scheme = "Bearer"
+            });
+            options.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    new string[] { }
+                }
+            });
+        });
+        return services;
     }
 }
