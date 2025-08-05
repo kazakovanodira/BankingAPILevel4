7636f7339fa4ba478bfebbebd85c9615bdf46293
Nodira Kazakova <kazakovanodira04@gmail.com>
2025-08-05 16:11:31 -0400

    added data annotations

--- Models/Requests/LoginRequest.cs
+++ Models/Requests/LoginRequest.cs
@@ -1,7 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace BankingAPILevel4.Models.Requests;
 
 public class LoginRequest
 {
+    [Required]
     public string Username { get; set; }
+    
+    [Required]
     public string Password { get; set; }
 }
